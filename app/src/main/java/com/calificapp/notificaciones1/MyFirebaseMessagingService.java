e7e7d9c73fb2c8f8450be4efe614a5a05ca02a76
Antonio Portada <antonio_portada25@hotmail.com>
2022-02-28 16:52:10 -0600

    carga de proyecto

---
+++ app/src/main/java/com/calificapp/notificaciones1/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.calificapp.notificaciones1;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
